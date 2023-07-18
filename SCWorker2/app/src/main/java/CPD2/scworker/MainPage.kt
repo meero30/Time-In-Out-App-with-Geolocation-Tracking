0f291cf223d54bf123be86b5ec4b65ea40e229fe
meeroDLSU <miro_hernandez@dlsu.edu.ph>
2023-07-18 23:34:35 +0800

    Added functions and pages related to permissions

--- SCWorker2/app/src/main/java/CPD2/scworker/MainPage.kt
+++ SCWorker2/app/src/main/java/CPD2/scworker/MainPage.kt
@@ -1,12 +1,16 @@
 package CPD2.scworker
 
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.net.Uri
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 
 
 class MainPage : AppCompatActivity() {
@@ -14,6 +18,7 @@ class MainPage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_page)
+
         val loginButton: Button = findViewById(R.id.btLogin)
         loginButton.setOnClickListener {
             val intent = Intent(this@MainPage, LoginPage::class.java)
@@ -28,6 +33,8 @@ class MainPage : AppCompatActivity() {
         }
 
     }
+
+
 }
 
 class LoginPage : AppCompatActivity() {
@@ -49,4 +56,75 @@ class LoginPage : AppCompatActivity() {
             startActivity(intent)
         }
     }
+}
+
+
+class TimeInTimeOutPage : AppCompatActivity() {
+
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+    private lateinit var tvLatitude: TextView
+    private lateinit var tvLongitude: TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.time_in_time_out_page)
+
+
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+
+    }
+
+    // TODO: Make the app monitor geofencing, if user disables permissions or location, automatic time out will execute.
+
+
+    //  TODO: Complete the getCurrentLocation() Function
+    private fun getCurrentLocation() {
+        // Check if permissions are enabled
+        if (checkPermissions()) {
+
+            if (isLocationEnabled()) {
+                // Get location here
+            } else {
+                // Open settings here to enable location
+            }
+        } else {
+            // Pop Up explaining the importance of Enabling permissions
+            // Request Permission
+        }
+    }
+
+
+    companion object {
+        private const val PERMISSION_REQUEST_ACCESS_LOCATION = 100
+    }
+
+
+    private fun checkPermissions(): Boolean {
+
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                android.Manifest.permission.ACCESS_COARSE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+            && ActivityCompat.checkSelfPermission(
+                this,
+                android.Manifest.permission.ACCESS_FINE_LOCATION
+            )
+            == PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this,
+                android.Manifest.permission.ACCESS_BACKGROUND_LOCATION
+            )
+            == PackageManager.PERMISSION_GRANTED
+        ) {
+
+            return true
+        }
+
+        return false
+    }
+
+
+    private fun isLocationEnabled(): Boolean {
+        return true
+    }
+
 }
