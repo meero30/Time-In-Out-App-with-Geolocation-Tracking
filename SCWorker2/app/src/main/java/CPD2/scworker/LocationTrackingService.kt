83c46472a8892573b08ffba48a34c71399d077f4
meeroDLSU <miro_hernandez@dlsu.edu.ph>
2023-08-02 20:38:46 +0800

    Working Location, but bugged when app is killed

---
+++ SCWorker2/app/src/main/java/CPD2/scworker/LocationTrackingService.kt
@@ -0,0 +1,159 @@
+package CPD2.scworker
+
+import CPD2.scworker.TimeInTimeOutPage.Companion.NOTIFICATION_CHANNEL_ID
+import android.app.NotificationManager
+import android.app.Service
+import android.content.Context
+import android.content.Intent
+import android.location.Location
+import android.os.CountDownTimer
+import android.os.IBinder
+import androidx.core.app.NotificationCompat
+import androidx.core.location.LocationManagerCompat.getCurrentLocation
+import androidx.localbroadcastmanager.content.LocalBroadcastManager
+import java.util.Timer
+import java.util.TimerTask
+
+class LocationTrackingService : Service() {
+
+    private val timer = Timer()
+    private var isOutsideGeofence = false
+    private var isFiveMinuteTimerInitialized = false
+
+    // 60000, 60000
+    private val oneMinuteTimer = object : CountDownTimer(5000, 5000) {
+        override fun onTick(millisUntilFinished: Long) {
+            // This won't be called since the interval is set to the full duration
+        }
+        override fun onFinish() {
+            if (isUserOutsideGeofence()) {
+                sendGeofenceAlert("You are outside the Geofence. You will be timed out within five minutes", true, 2)
+                startFiveMinuteTimer()
+                isFiveMinuteTimerInitialized = true
+            }
+        }
+    }
+    // 300000 , 300000
+    private val fiveMinuteTimer = object : CountDownTimer(5000, 5000) {
+        override fun onTick(millisUntilFinished: Long) {
+            // This won't be called since the interval is set to the full duration
+        }
+
+        override fun onFinish() {
+            if (isUserOutsideGeofence()) {
+                sendGeofenceAlert("Auto time-out initiated", false, 3)
+                callExternalFunction()
+            }
+        }
+    }
+
+
+    override fun onBind(intent: Intent?): IBinder? {
+        return null
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        startForegroundServiceWithNotification()
+        sendGeofenceAlert("Starting Foreground Service", false, 4)
+        // Start timer task to check geofence status
+        timer.scheduleAtFixedRate(object : TimerTask() {
+            override fun run() {
+                checkGeofenceStatus()
+            }
+        }, 0, 10000) // Check every 10 seconds
+
+        return START_STICKY
+    }
+
+    private fun startForegroundServiceWithNotification() {
+        val notification = NotificationCompat.Builder(this, NOTIFICATION_CHANNEL_ID)
+            .setContentTitle("Location Tracking")
+            .setContentText("Tracking your location for geofence...")
+            .setSmallIcon(R.drawable.ic_launcher_foreground) // Your app icon
+            .setOngoing(true)
+            .build()
+
+        startForeground(1, notification)
+    }
+
+    private fun checkGeofenceStatus() {
+        if (isUserOutsideGeofence()) { // Your geofence checker function
+            if (!isOutsideGeofence) {
+                isOutsideGeofence = true
+                startOutsideGeofenceTimer()
+                // Send another notification or updv ate the current one
+            }
+        } else {
+            isOutsideGeofence = false
+            // Reset or stop the timer for outside geofence
+            oneMinuteTimer.cancel()
+            fiveMinuteTimer.cancel()
+
+            // Notifies user once they are back at the geofence
+            if (isFiveMinuteTimerInitialized) {
+                sendGeofenceAlert("You are inside the Geofence, any time-out operations will be cancelled", false, 3)
+                isFiveMinuteTimerInitialized = false
+            }
+
+            dismissNotification(2) // Dismissing non-removable notification
+        }
+    }
+
+    private fun isUserOutsideGeofence(): Boolean {
+        // Your custom geofence checker logic here
+        var distanceInMeters : FloatArray = floatArrayOf(0f)
+
+        Location.distanceBetween(assignedLatitude, assignedLongitude, currentLatitude, currentLongitude, distanceInMeters)
+        var placeholder : String = ""
+
+        if (distanceInMeters[0].toDouble() < radius) {
+            // User is inside the Geo-fence
+            return false
+        }
+        return true
+    }
+
+    private fun sendGeofenceAlert(message : String, onGoingValue : Boolean, idNum : Int) {
+        val notification = NotificationCompat.Builder(this, NOTIFICATION_CHANNEL_ID)
+            .setContentTitle("Geofence Alert")
+            .setContentText(message)
+            .setSmallIcon(R.drawable.ic_launcher_foreground) // Your app icon
+            .setOngoing(onGoingValue)
+            .build()
+
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.notify(idNum, notification) // 2 is the notification id for this particular notification
+    }
+
+
+    private fun startOutsideGeofenceTimer() {
+        oneMinuteTimer.start()
+    }
+
+    private fun startFiveMinuteTimer() {
+        fiveMinuteTimer.start()
+    }
+
+    private fun dismissNotification(idNum: Int) {
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.cancel(idNum) // 2 is the notification id for the outside geofence alert
+    }
+
+
+    override fun onDestroy() {
+        super.onDestroy()
+        timer.cancel()
+        dismissNotification(1) // Dismissing the "You are being tracked" Notification
+        // Handle any other cleanup
+    }
+
+    companion object {
+        const val ACTION_USER_OUTSIDE_GEOFENCE = "com.CPD2.scworker.ACTION_USER_OUTSIDE_GEOFENCE"
+    }
+
+    private fun callExternalFunction() {
+        val intent = Intent(ACTION_USER_OUTSIDE_GEOFENCE)
+        LocalBroadcastManager.getInstance(this).sendBroadcast(intent)
+    }
+
+}
--- SCWorker2/app/src/main/java/CPD2/scworker/TimeInTimeOutPage.kt
+++ SCWorker2/app/src/main/java/CPD2/scworker/TimeInTimeOutPage.kt
@@ -1,31 +1,54 @@
 package CPD2.scworker
 
 import android.Manifest
-import android.app.PendingIntent
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.content.IntentFilter
 import android.content.pm.PackageManager
+import android.location.Location
 import android.location.LocationManager
+import android.os.Build
 import android.os.Bundle
 import android.provider.Settings
+import android.widget.Button
 import android.widget.EditText
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.Geofence
-import com.google.android.gms.location.GeofencingClient
-import com.google.android.gms.location.GeofencingRequest
 import com.google.android.gms.location.LocationCallback
 import com.google.android.gms.location.LocationRequest
 import com.google.android.gms.location.LocationResult
 import com.google.android.gms.location.LocationServices
+import com.instacart.library.truetime.TrueTimeRx
+import io.reactivex.Single
+import io.reactivex.disposables.Disposable
+import io.reactivex.schedulers.Schedulers
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
+import android.os.Handler
+import android.os.Looper
+import android.view.View
+import androidx.localbroadcastmanager.content.LocalBroadcastManager
 
-//data class LocationData(val latitude: Double, val longitude: Double)
-//
+
 //const val  GEOFENCE_RADIUS_IN_METERS: Float = 100.0F
 //const val  GEOFENCE_EXPIRATION_IN_MILLISECONDS: Float = 8 * 60 * 60 * 1000F // 8 hours in milliseconds
+var currentLatitude : Double = 0.0
+var currentLongitude : Double = 0.0
+
+// Washington National Cathedral 38.8951 and longitude -77.0364
+var assignedLatitude : Double =  38.9072
+var assignedLongitude : Double = -77.0369
+var radius : Double = 1000.0 // In meters
+
 
 
 class TimeInTimeOutPage : AppCompatActivity() {
@@ -33,88 +56,212 @@ class TimeInTimeOutPage : AppCompatActivity() {
     private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
     private lateinit var tvLatitude: TextView
     private lateinit var tvLongitude: TextView
-//    private lateinit var geofencingClient: GeofencingClient
     private lateinit var etLatitude : EditText
     private lateinit var etLongitude : EditText
-//    private lateinit var coordinates: LocationData
     private lateinit var geofenceList : MutableList<Geofence>
+    private lateinit var tvGeofenceResults : TextView
+    private lateinit var locationCallback: LocationCallback
+    private lateinit var timeInButton: Button
+    private lateinit var timeOutButton: Button
+    private var disposable: Disposable? = null
 
-//    private val geofencePendingIntent: PendingIntent by lazy {
-//        val intent = Intent(this, GeofenceBroadcastReceiver::class.java)
-//        // We use FLAG_UPDATE_CURRENT so that we get the same pending intent back when calling
-//        // addGeofences() and removeGeofences().
-//        PendingIntent.getBroadcast(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
-//    }
+    private val geofenceReceiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context?, intent: Intent?) {
+            when(intent?.action) {
+                LocationTrackingService.ACTION_USER_OUTSIDE_GEOFENCE -> {
+                    // Call your function here (autoTimeOut)
+                    autoTimeOut()
+                }
+            }
+        }
+    }
 
+    companion object {
+        private const val PERMISSION_REQUEST_ACCESS_LOCATION = 100
+        const val NOTIFICATION_CHANNEL_ID = "geofence_channel_id"
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.time_in_time_out_page)
 
-//        geofencingClient = LocationServices.getGeofencingClient(this)
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
 
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = "Geofence Notifications"
+            val descriptionText = "Notifications related to geofencing events"
+            val importance = NotificationManager.IMPORTANCE_HIGH
+            val channel = NotificationChannel(NOTIFICATION_CHANNEL_ID, name, importance).apply {
+                description = descriptionText
+            }
+
+            // Register the channel with the system
+            val notificationManager: NotificationManager =
+                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
+
+        val imageView: ImageView = findViewById(R.id.logo2)
+        imageView.setOnClickListener {
+            onBackPressed()
+        }
+
+        val updateResults: Button = findViewById(R.id.btUpdateResults)
+        updateResults.setOnClickListener {
+            isInsideGeoFence(assignedLatitude, assignedLongitude, currentLatitude, currentLongitude, radius)
+        }
+
+        val resetTimeButtons : Button = findViewById(R.id.btResetTimer)
+//        resetTimeButtons.setOnClickListener{
+//            timeInButton.isClickable = true
+//            timeInButton.alpha = 1.0f
+//            timeOutButton.isClickable = false
+//            timeOutButton.alpha = 0.5f
+//        }
+
+
+
+        timeInButton  = findViewById(R.id.btTimeIn)
+        timeInButton.setOnClickListener {
+            // Check if location is within geofence
+            getCurrentLocation()
+            Handler(Looper.getMainLooper()).postDelayed({
+                // This block of code will be executed after 7.5 seconds
+                if (isInsideGeoFence(assignedLatitude, assignedLongitude, currentLatitude, currentLongitude, radius)) {
+                    // if within geofence, get time and date and proceed to send data to php server
+                    startLocationService()
+                    var date =  getCurrentTimeAndDate()
+                    // send data
+                    Toast.makeText(this, "Timed-In Successfully", Toast.LENGTH_SHORT).show()
+                    handleTimeIn()
+                } else {
+                    // if not within geofence, send a pop up that warns the user that they need to be withing the geofence
+                    stopLocationUpdates()
+                    Toast.makeText(this, "Time-In Failure", Toast.LENGTH_SHORT).show()
+
+                }
+            }, 7500)
+
+
+        }
+
+        timeOutButton = findViewById(R.id.btTimeOut)
+        timeOutButton.setOnClickListener {
+            // calling stop location updates first because locationCallback Global var will be replaced with a new instantiation.
+            // This fixes the bug that location keeps updating due to it being replaced before being stopped. I'm assuming each instantiation has different ID's
+            stopLocationUpdates()
+            getCurrentLocation()  // sets up permissions, location updates, and edits global variables for the user lat an lng
+            Handler(Looper.getMainLooper()).postDelayed({
+                // This block of code will be executed after 7.5 seconds
+
+                // Check if location is within geofence
+                if (isInsideGeoFence(assignedLatitude, assignedLongitude, currentLatitude, currentLongitude, radius)) {
+                    // if within geofence, get time and date and proceed to send data to php server
+                    var date =  getCurrentTimeAndDate()
+                    // send data
+
+                    stopLocationService()
+                    stopLocationUpdates()
+                    Toast.makeText(this, "Timed-Out Successfully", Toast.LENGTH_SHORT).show()
+                    handleTimeOut()
+
+                } else {
+                    // if not within geofence, send a pop up that warns the user that they need to be withing the geofence
+                    stopLocationUpdates()
+                    Toast.makeText(this, "Time-Out Failure", Toast.LENGTH_SHORT).show()
+                }
+            }, 7500)
+
+        }
+
+        // Set initial state
+        val sharedPreferences = getSharedPreferences("app_prefs", Context.MODE_PRIVATE)
+        val isTimeInEnabled = sharedPreferences.getBoolean("isTimeInEnabled", true)
+
+        if (isTimeInEnabled) {
+            timeInButton.isClickable = true
+            timeInButton.alpha = 1.0f
+            timeOutButton.isClickable = false
+            timeOutButton.alpha = 0.5f
+        } else {
+            timeInButton.isClickable = false
+            timeInButton.alpha = 0.5f
+            timeOutButton.isClickable = true
+            timeOutButton.alpha = 1.0f
+        }
+
+        
         // Get GeoFence coordinates lat,long
         etLatitude = findViewById(R.id.etLatitude)
         etLongitude = findViewById(R.id.etLongitude)
-//
-//        var assignedLatitude = etLatitude.text.toString().toDouble()
-//        var assignedLongitude = etLongitude.text.toString().toDouble()
-
-//        coordinates = LocationData(assignedLatitude,assignedLongitude)
-//
-//        val geofenceData: Map<String, LocationData> = mapOf(
-//            "geofence_id_1" to coordinates,  // assigned coordinates
-//        )
-
-//        addToGeofenceList()
-
-        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
 
         tvLatitude = findViewById(R.id.tvLatitude)
         tvLongitude = findViewById(R.id.tvLongitude)
-        getCurrentLocation()
 
+        tvGeofenceResults = findViewById(R.id.tvGeofenceResults)
 
 
     }
 
-//    private fun getGeofencingRequest(): GeofencingRequest {
-//        return GeofencingRequest.Builder().apply {
-//            setInitialTrigger(GeofencingRequest.INITIAL_TRIGGER_ENTER)
-//            addGeofences(geofenceList)
-//        }.build()
-//    }
+    private fun popUpWarning() {
 
-//    private fun addToGeofenceList() {
-//        geofenceList.add(Geofence.Builder()
-//            // Set the request ID of the geofence. This is a string to identify this
-//            // geofence.
-//            .setRequestId("geofence_id_1")
-//
-//            // Set the circular region of this geofence.
-//            .setCircularRegion(
-//                coordinates.latitude,
-//                coordinates.latitude,
-//                GEOFENCE_RADIUS_IN_METERS
-//            )
-//            // Set the expiration duration of the geofence. This geofence gets automatically
-//            // removed after this period of time.
-//            .setExpirationDuration(GEOFENCE_EXPIRATION_IN_MILLISECONDS.toLong())
-//
-//            // Set the transition types of interest. Alerts are only generated for these
-//            // transition. We track entry and exit transitions in this sample.
-//            .setTransitionTypes(Geofence.GEOFENCE_TRANSITION_ENTER or Geofence.GEOFENCE_TRANSITION_EXIT)
-//
-//            // Create the geofence.
-//            .build())
-//    }
+    }
+
+    private fun getCurrentTimeAndDate(): Single<String> {
+        val format = SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.getDefault())
+        return Single.create { emitter ->
+            try {
+                TrueTimeRx.build()
+                    .initializeRx("time.google.com")
+                    .subscribeOn(Schedulers.io())
+                    .subscribe(
+                        { date ->
+                            emitter.onSuccess(format.format(date))
+                        },
+                        { error ->
+                            emitter.onSuccess(format.format(Date()))
+                        }
+                    )
+                    .also { disposable = it } // store the Disposable
+            } catch (e: Exception) {
+                emitter.onError(e)
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy() // or any other appropriate lifecycle event
+        disposable?.dispose()
+    }
+
+    fun handleTimeIn() {
+        timeInButton.isClickable = false
+        timeInButton.alpha = 0.5f
+
+        timeOutButton.isClickable = true
+        timeOutButton.alpha = 1.0f
+
+        saveButtonState(false)  // Saving that the Time In button is not enabled
+    }
+
+    fun handleTimeOut() {
+        timeOutButton.isClickable = false
+        timeOutButton.alpha = 0.5f
+
+        timeInButton.isClickable = true
+        timeInButton.alpha = 1.0f
+
+        saveButtonState(true)  // Saving that the Time In button is enabled
+    }
+
+    fun saveButtonState(isTimeInEnabled: Boolean) {
+        val sharedPreferences = getSharedPreferences("app_prefs", Context.MODE_PRIVATE)
+        val editor = sharedPreferences.edit()
+        editor.putBoolean("isTimeInEnabled", isTimeInEnabled)
+        editor.apply()
+    }
 
 
-
-
-
-
-    //  TODO: Complete the getCurrentLocation() Function
     private fun getCurrentLocation() {
         // Check if permissions are enabled
         if (checkPermissions()) {
@@ -141,15 +288,19 @@ class TimeInTimeOutPage : AppCompatActivity() {
                 locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
 
 
-                val locationCallback: LocationCallback = object : LocationCallback() {
+                locationCallback = object : LocationCallback() {
                     override fun onLocationResult(locationResult: LocationResult) {
                         for (location in locationResult.locations) {
                             if (location != null) {
 
                                 // Such as:
-                                Toast.makeText(this@TimeInTimeOutPage, "Location Successfully Retrieved", Toast.LENGTH_SHORT).show()
+//                                Toast.makeText(this@TimeInTimeOutPage, "Location Successfully Retrieved", Toast.LENGTH_SHORT).show()
                                 tvLatitude.text = "" + location.latitude
                                 tvLongitude.text = "" + location.longitude
+
+                                currentLatitude = location.latitude
+                                currentLongitude = location.longitude
+
                             }
                             else {
                                 Toast.makeText(this@TimeInTimeOutPage, "location is Null", Toast.LENGTH_SHORT).show()
@@ -174,6 +325,12 @@ class TimeInTimeOutPage : AppCompatActivity() {
         }
     }
 
+    private fun stopLocationUpdates() {
+        if (::locationCallback.isInitialized) {
+            fusedLocationProviderClient.removeLocationUpdates(locationCallback)
+        }
+    }
+
     private fun requestPermission() {
 
         ActivityCompat.requestPermissions(
@@ -186,17 +343,15 @@ class TimeInTimeOutPage : AppCompatActivity() {
 
     }
 
-    fun createLocationRequest() {
-        val locationRequest = LocationRequest.create();
-        locationRequest.setInterval(10000);
-        locationRequest.setFastestInterval(5000);
-        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
 
+    private fun startLocationService() {
+        val intent = Intent(this, LocationTrackingService::class.java)
+        startService(intent)
     }
 
-
-    companion object {
-        private const val PERMISSION_REQUEST_ACCESS_LOCATION = 100
+    private fun stopLocationService() {
+        val intent = Intent(this, LocationTrackingService::class.java)
+        stopService(intent)
     }
 
 
@@ -248,5 +403,53 @@ class TimeInTimeOutPage : AppCompatActivity() {
     }
 
     // TODO: Make the app monitor geofencing, if user disables permissions or location, automatic time out will execute.
+    private fun autoTimeOut() {
+        getCurrentLocation()
+        Handler(Looper.getMainLooper()).postDelayed({
+            // This block of code will be executed after 7.5 seconds
+
+            var date =  getCurrentTimeAndDate()
+            // send data
+            stopLocationService()
+            stopLocationUpdates()
+            Toast.makeText(this, "Auto Timed Out", Toast.LENGTH_SHORT).show()
+            handleTimeOut()
+        }, 7500)
+
+    }
+
+    private fun isInsideGeoFence(assignedLatitude: Double, assignedLongitude: Double, currentLatitude: Double, currentLongitude : Double, radius : Double): Boolean {
+
+        var distanceInMeters : FloatArray = floatArrayOf(0f)
+
+        Location.distanceBetween(assignedLatitude, assignedLongitude, currentLatitude, currentLongitude, distanceInMeters)
+        var placeholder : String = ""
+
+        if (distanceInMeters[0].toDouble() < radius) {
+            // User is inside the Geo-fence
+//            showNotificationEvent.call()
+            placeholder = "User is inside Geofence " + distanceInMeters[0].toString()
+            tvGeofenceResults.text = placeholder
+            return true
+
+        } else {
+            // User is outside Geo-fence
+            placeholder = "User is outside Geofence " + distanceInMeters[0].toString()
+            tvGeofenceResults.text = placeholder
+            return false
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        LocalBroadcastManager.getInstance(this)
+            .registerReceiver(geofenceReceiver, IntentFilter(LocationTrackingService.ACTION_USER_OUTSIDE_GEOFENCE))
+    }
+
+    override fun onPause() {
+        super.onPause()
+        LocalBroadcastManager.getInstance(this).unregisterReceiver(geofenceReceiver)
+    }
+
 
 }
